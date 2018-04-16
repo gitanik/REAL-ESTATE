95bed1dc6d1985280e82b0b1005ad0a45b33abdd
gitanik <37584526+gitanik@users.noreply.github.com>
2018-04-16 16:56:31 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
+}
+
+$('ol li').bind('click', function(){
+
+        var index = $(this).index() + 1;
+
+        $(".active").fadeOut(300);
+
+        $("#slide_" + index).fadeIn(300);        
+        $(".slide").removeClass("active");
+        $("#slide_" + index).addClass("active");
+});
